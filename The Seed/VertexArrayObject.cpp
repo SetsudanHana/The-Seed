1d1b15864797de0cd1e1c8b8e0540690d2325932
Mateusz Wręczycki <setsudan.hana@gmail.com>
2016-06-07 19:57:53 +0200

    get VAO model definition

---
+++ The Seed/VertexArrayObject.cpp
@@ -0,0 +1,66 @@
+#include "VertexArrayObject.h"
+
+static std::string TAG = "VertexArrayObject";
+
+struct VertexArrayObjectDeleter {
+	inline void operator()(unsigned* vaoId) {
+		std::stringstream out;
+		out << "Remove VAO: " << *vaoId;
+		utils::Log::Instance()->logDebug(TAG, out.str());
+		glDeleteVertexArrays(1, vaoId);
+	}
+};
+
+gl::VertexArrayObject::VertexArrayObject(){
+}
+
+gl::VertexArrayObject::VertexArrayObject(unsigned & vaoId){
+	std::unique_ptr<unsigned int, std::function<void(unsigned int*)>> ptr(&vaoId, VertexArrayObjectDeleter());
+	mVaoId = std::move(ptr);
+}
+
+gl::VertexArrayObject::VertexArrayObject(const VertexArrayObject &){
+}
+
+gl::VertexArrayObject::~VertexArrayObject(){
+}
+
+void gl::VertexArrayObject::bindVbo(std::shared_ptr<VertexBufferObject> vbo) {
+	glBindVertexArray(*mVaoId);
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo->mVboId);
+
+	switch (vbo->getType()) {
+		case VertexBufferObject::TEXTURE_COORDS:
+		case VertexBufferObject::POSITION_2:
+			glVertexAttribPointer(vbo->getType(), 2, GL_FLOAT, GL_FALSE, 0, NULL);
+			break;
+	}
+
+	std::stringstream out;
+	out << "VertexBuffer id: " << *vbo->mVboId << " binded to VertexArray id: " << *mVaoId;
+	utils::Log::Instance()->logDebug(TAG, out.str());
+
+	enableVBO(vbo->getType());
+}
+
+void gl::VertexArrayObject::useVAO(){
+	glBindVertexArray(*mVaoId);
+}
+
+void gl::VertexArrayObject::enableVBO(const VertexBufferObject::VertexBufferObjectType & type){
+	glBindVertexArray(*mVaoId);
+	glEnableVertexAttribArray(type);
+
+	std::stringstream out;
+	out << "VertexBufferType: " << type << " enabled in VertexArray id: " << *mVaoId;
+	utils::Log::Instance()->logDebug(TAG, out.str());
+}
+
+void gl::VertexArrayObject::disableVBO(const VertexBufferObject::VertexBufferObjectType & type){
+	glBindVertexArray(*mVaoId);
+	glDisableVertexAttribArray(type);
+
+	std::stringstream out;
+	out << "VertexBufferType: " << type << " disabled in VertexArray id: " << *mVaoId;
+	utils::Log::Instance()->logDebug(TAG, out.str());
+}
---
+++ The Seed/VertexArrayObject.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include "VertexBufferObject.h"
+#include "Log.h"
+
+namespace gl {
+
+	class VertexArrayObject {
+
+		public:
+			VertexArrayObject();
+			VertexArrayObject(unsigned&);
+			VertexArrayObject(const VertexArrayObject&);
+			~VertexArrayObject();
+
+			void bindVbo(std::shared_ptr<VertexBufferObject> vbo);
+			void useVAO();
+
+			void enableVBO(const VertexBufferObject::VertexBufferObjectType& type);
+			void disableVBO(const VertexBufferObject::VertexBufferObjectType& type);
+
+		private:
+			std::unique_ptr<unsigned, std::function<void(unsigned int*)>> mVaoId;
+	};
+
+}
