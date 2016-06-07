267a1fd5f92ee0c1e9c94567f538140b3878f0ed
Mateusz Wręczycki <setsudan.hana@gmail.com>
2016-06-07 12:36:16 +0200

    add additional logging to Texture model

--- The Seed/Texture.cpp
+++ The Seed/Texture.cpp
@@ -28,6 +28,9 @@ gl::Texture::~Texture(){
 }
 
 void gl::Texture::useTexture(const unsigned & sampler){
+	std::stringstream out;
+	out << "Using texture with id: " << *mTextureId.get() << ", for sampler: " << sampler;
+	utils::Log::Instance()->logDebug(TAG, out.str());
 	glActiveTexture(GL_TEXTURE0 + sampler);
 	glBindTexture(GL_TEXTURE_2D, *mTextureId.get());
 }
